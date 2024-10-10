6ae92df36fc90f928dfb905e6ae1e064ec00429d
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-10-10 14:58:52 +0530

    city page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Weather from "./pages/Weather.jsx";
 import SevenDay from "./components/SevenDay.jsx";
+import City from "./pages/City.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <div className="grid grid-cols-1 md:grid-cols-6 gap-4 sm:gap-5 md:gap-6 xl:gap-8 my-5 ">
         <Routes>
           <Route path="/" element={<Weather />} />
+          <Route path="/city" element={<City />} />
         </Routes>
         <SevenDay />
       </div>
---
+++ src/pages/City.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const City = () => {
+  return (
+    <div>City</div>
+  )
+}
+
+export default City

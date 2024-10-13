973332629df1097256bc2f477797a7475198e13c
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-10-14 03:58:12 +0530

    fided a small alignment

--- src/pages/Others.jsx
+++ src/pages/Others.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Others = () => {
   return (
-    <div className="md:col-span-6 flax items-center justify-center">
+    <div className="md:col-span-4 flax items-center justify-center">
       <h1 className="font-bold text-[50px]">
         Thank you for visiting my webpage
       </h1>
